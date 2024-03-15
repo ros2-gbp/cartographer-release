f2d6a5c9f44b18dfcbcf7ea8842191e1978e5bc3
Chris Lalancette <clalancette@gmail.com>
2024-03-15 14:32:11 +0000

    Rebase from 'release/humble/cartographer'

--- cartographer/mapping/internal/optimization/ceres_pose.cc
+++ cartographer/mapping/internal/optimization/ceres_pose.cc
@@ -27,6 +27,19 @@ CeresPose::Data FromPose(const transform::Rigid3d& pose) {
                            pose.rotation().y(), pose.rotation().z()}}};
 }
 
+#if CERES_VERSION_MAJOR > 2 || CERES_VERSION_MAJOR == 2 && CERES_VERSION_MINOR >= 1
+CeresPose::CeresPose(
+    const transform::Rigid3d& pose,
+    std::unique_ptr<ceres::Manifold> translation_manifold,
+    std::unique_ptr<ceres::Manifold> rotation_manifold,
+    ceres::Problem* problem)
+    : data_(std::make_shared<CeresPose::Data>(FromPose(pose))) {
+  problem->AddParameterBlock(data_->translation.data(), 3,
+                             translation_manifold.release());
+  problem->AddParameterBlock(data_->rotation.data(), 4,
+                             rotation_manifold.release());
+}
+#else
 CeresPose::CeresPose(
     const transform::Rigid3d& pose,
     std::unique_ptr<ceres::LocalParameterization> translation_parametrization,
@@ -38,6 +51,7 @@ CeresPose::CeresPose(
   problem->AddParameterBlock(data_->rotation.data(), 4,
                              rotation_parametrization.release());
 }
+#endif
 
 const transform::Rigid3d CeresPose::ToRigid() const {
   return transform::Rigid3d::FromArrays(data_->rotation, data_->translation);
